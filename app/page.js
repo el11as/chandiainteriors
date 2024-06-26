874f5840c83bc13dcba1ffb064c64d0b632748d8
Elias Gomez Fuentes <elias.gomezfuentes@gmail.com>
2024-06-26 19:35:00 -0400

    instagram section

--- app/page.js
+++ app/page.js
@@ -287,7 +287,7 @@ function InstagramSection () {
           ].map((item, index) => (
             <div className="relative group">
               <div
-                  key={item.src}
+                  key={index}
                   alt={item.alt}
                   className={clsx(
                     'tive aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800rela',
